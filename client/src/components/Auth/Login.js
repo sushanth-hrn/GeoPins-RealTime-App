d7240f1b81ea03c5814ba8754ae3d54131b309d6
sushanth <sushanth.nukala14@gmail.com>
2021-06-22 16:52:20 +0530

    setting up google plus OAuth on client side using gcp and react-google-login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
+
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+
+  const onSuccess = (googleUser) => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log({idToken});
+  }
+
+  return (
+    <GoogleLogin 
+      clientId="355270680604-85hertmsivpt0ra9cigs42a9gq4u9700.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
