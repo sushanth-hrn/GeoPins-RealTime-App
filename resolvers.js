e6caec76abac391e0e502adb893d3e2140049b3e
sushanth <sushanth.nukala14@gmail.com>
2021-06-22 22:37:28 +0530

    small addition for showing authentication error if usr not authenticated

--- resolvers.js
+++ resolvers.js
@@ -8,9 +8,9 @@ const user = {
 }
 
 const authenticated = next => (root, args, ctx, info) => {
-    // if(!ctx.currentUser){
-    //     throw new AuthenticationError('You must be logged in');
-    // }
+    if(!ctx.currentUser){
+        throw new AuthenticationError('You must be logged in');
+    }
     return next(root, args, ctx, info);
 }
 
