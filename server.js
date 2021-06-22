84f1a7c988ff69b29dd2b1877d5d3c87207927b4
sushanth <sushanth.nukala14@gmail.com>
2021-06-22 12:54:07 +0530

    connecting to mongodb atlas

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+    .connect(process.env.MONGO_URI,{ useNewUrlParser:true })
+    .then(() => console.log('Connected to Database'))
+    .catch((err) => console.error(err));
+
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
