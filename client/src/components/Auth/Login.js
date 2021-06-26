f0c3c2807c50dd2b16aafcfd6e10dc15eae22219
sushanth <sushanth.nukala14@gmail.com>
2021-06-26 23:54:00 +0530

    app cleanup and adding onFailure handler to GoogleLogin button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -3,40 +3,50 @@ import { GoogleLogin } from "react-google-login";
 import { GraphQLClient } from 'graphql-request';
 
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
 import Context from "../../context";
-
-const ME_QUERY = `
-{
-  me{
-    _id
-    name
-    email
-    picture
-  }
-}
-`;
+import { ME_QUERY } from '../../graphql/queries';
 
 const Login = ({ classes }) => {
   const { dispatch } = useContext(Context);
 
   const onSuccess = async (googleUser) => {
-    const idToken = googleUser.getAuthResponse().id_token;
-    const client = new GraphQLClient('http://localhost:4000', {
-      headers: { authorization: idToken }
-    });
-    console.log({ idToken });
-    const data = await client.request(ME_QUERY);
-    dispatch({ type: 'LOGIN_USER', payload: data.me });
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient('http://localhost:4000', {
+        headers: { authorization: idToken }
+      });
+      console.log({ idToken });
+      const { me } = await client.request(ME_QUERY);
+      dispatch({ type: 'LOGIN_USER', payload: me });
+    } catch (error) {
+      onFailure(error);
+    }
+  }
+
+  const onFailure = err => {
+    console.error('Error logging in the user ', err);
   }
 
   return (
-    <GoogleLogin
-      clientId="355270680604-85hertmsivpt0ra9cigs42a9gq4u9700.apps.googleusercontent.com"
-      onSuccess={onSuccess}
-      isSignedIn={true}
-    />
+    <div className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66,133,244)" }}
+      >
+        Welcome
+      </Typography>
+      <GoogleLogin
+        clientId="355270680604-85hertmsivpt0ra9cigs42a9gq4u9700.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+      />
+    </div>
   );
 };
 
