0b0311bb737fbfc79ef9b1dcae913cf721cdb7c8
sushanth <sushanth.nukala14@gmail.com>
2021-06-27 10:47:07 +0530

    redirecting logged in users to app page

--- client/src/context.js
+++ client/src/context.js
@@ -1,7 +1,8 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 const Context = createContext({
-    currentUser: null
+    currentUser: null,
+    isAuth: false
 });
 
 export default Context;
