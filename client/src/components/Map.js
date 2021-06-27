23065b532c0703414633f365d675f4aea598a929
sushanth <sushanth.nukala14@gmail.com>
2021-06-27 14:04:11 +0530

    Placing map on screen using mapbox api

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import Button from "@material-ui/core/Button";
-// import Typography from "@material-ui/core/Typography";
-// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
 
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapboxApiAccessToken="pk.eyJ1Ijoic3VzaGFudGgtaHJuIiwiYSI6ImNrcTdpcms0aTA2a2Yyd29hYnQybTl3a2MifQ.hsf43FH9798nr1RG_Jgg8g"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onViewportChange={(newViewport) => setViewport(newViewport)}
+        {...viewport}
+      >
+        <div className={classes.navigationControl}>
+          <NavigationControl 
+            onViewportChange={(newViewport) => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
