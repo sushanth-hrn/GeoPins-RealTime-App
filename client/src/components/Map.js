20ce49b43f1951c9a175d1b0c7a396317c7e332f
sushanth <sushanth.nukala14@gmail.com>
2021-06-27 16:07:59 +0530

    Adding draft pin inorder to enable user to mark locations on map when he clicks somewhere on map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
 // import Button from "@material-ui/core/Button";
@@ -6,6 +6,7 @@ import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
 import PinIcon from './PinIcon';
+import Context from '../context';
 
 const INITIAL_VIEWPORT = {
   latitude: 37.7577,
@@ -16,6 +17,8 @@ const INITIAL_VIEWPORT = {
 const Map = ({ classes }) => {
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
   const [userPosition, setUserPostion] = useState(null);
+
+  const { state, dispatch } = useContext(Context);
 
   useEffect(() => {
     getUserPosition();
@@ -34,8 +37,29 @@ const Map = ({ classes }) => {
           latitude,
           longitude
         })
-      }); 
+      },(error) => {
+        console.error("Error Code = " + error.code + " - " + error.message);
+      }
+    ); 
     }
+  }
+
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if(!leftButton){
+      return;
+    }
+    if(!state.draft){
+      dispatch({ type:'CREATE_DRAFT' });
+    }
+    const [ longitude, latitude ] = lngLat;
+    dispatch({
+      type:'UPDATE_DRAFT_LOCATION', 
+      payload:{
+        latitude,
+        longitude
+      }
+    })
+
   }
 
   return (
@@ -46,6 +70,7 @@ const Map = ({ classes }) => {
         mapboxApiAccessToken="pk.eyJ1Ijoic3VzaGFudGgtaHJuIiwiYSI6ImNrcTdpcms0aTA2a2Yyd29hYnQybTl3a2MifQ.hsf43FH9798nr1RG_Jgg8g"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         onViewportChange={(newViewport) => setViewport(newViewport)}
+        onClick={handleMapClick}
         {...viewport}
       >
         <div className={classes.navigationControl}>
@@ -61,6 +86,16 @@ const Map = ({ classes }) => {
             offsetTop={-37}
           >
             <PinIcon size={40} color="red" />
+          </Marker>
+        }
+        {state.draft && 
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="hotpink" />
           </Marker>
         }
       </ReactMapGL>
