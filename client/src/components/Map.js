8f54a467bf7c72b1e73611d054b9063a2494d05b
sushanth_hrn <sushanth.nukala14@gmail.com>
2021-07-12 23:39:25 +0530

    showing the prviously added pins on the map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -55,7 +55,7 @@ const Map = ({ classes }) => {
 
   const getPins = async () => {
     const { getPins } = await client.request(GET_PINS_QUERY);
-    console.log({ getPins });
+    dispatch({ type : "GET_PINS", payload : getPins});
   }
 
   const handleMapClick = ({ lngLat, leftButton }) => {
@@ -111,6 +111,19 @@ const Map = ({ classes }) => {
           >
             <PinIcon size={40} color="hotpink" />
           </Marker>
+        }
+        {
+          state.pins.map((pin) => {
+            return <Marker
+              key={pin._id}
+              latitude={pin.latitude}
+              longitude={pin.longitude}
+              offsetLeft={-19}
+              offsetTop={-37}
+            >
+            <PinIcon size={40} color="darkblue" />
+          </Marker>
+          })
         }
       </ReactMapGL>
       <Blog />
--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -36,8 +36,9 @@ const CreatePin = ({ classes }) => {
         latitude,
         longitude
       }
-      const { createPin } = await client.request( CREATE_PIN_MUTATION, variables)
-      console.log("Pin craeted ", { createPin });
+      const { createPin } = await client.request(CREATE_PIN_MUTATION, variables)
+      console.log("Pin created ", { createPin });
+      dispatch({ type : "CREATE_PIN", payload : createPin});
       handleDeleteDraft();
     } catch (error) {
       setSubmitting(false);
